a280b78f6d27e90fcb8454ee46dd8ed911ebf555
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-01 19:05:26 +0900

    update main_controller.py

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -8,26 +8,4 @@ def index():
 @main.route("/addtask", methods=["GET","POST"])
 def add_task():
     return render_template("addtask.html")
-
-@main.route("/form", methods=["GET","POST"])
-def form():
-    if request.method == "GET":
-        return render_template("form.html")
-    else:     
-        myname = request.form.get("myname")
-        token = request.form.get("token")
-        session["myname"] = myname
-        return redirect("/confirm")
-
-@main.route("/confirm", methods=["GET","POST"])
-def confirm():
-    if request.method == "GET":
-        myname = session["myname"]
-        return render_template("confirm.html", myname = myname)
-    else:
-        session.clear()
-        return redirect("/complete")
-    
-@main.route("/complete")
-def complete():
     return render_template("complete.html")
