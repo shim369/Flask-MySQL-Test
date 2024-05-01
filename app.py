9975b7cd374db241a073c7f83ebf03b403d9736c
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-01 18:51:52 +0900

    update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from config import Config
+from extensions import db
 from controllers.main_controller import main
+from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.register_blueprint(main)
-app.secret_key = "abcd1234"
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    register_resources(app)
+    register_extensions(app)
+    return app
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+def register_resources(app):
+    app.register_blueprint(main, url_prefix='/')
 
 if __name__ == "__main__":
-    #debug=False
-    app.run(debug=True)
+    app = create_app()
+    app.secret_key = "abcd1234"
+    app.run('127.0.0.1', 5000, debug=True)
