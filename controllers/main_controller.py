8a7f5521ddd9142c7fa9995a6de01e2663082004
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-02 11:47:31 +0900

    add get_all function

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,15 +1,17 @@
-from flask import Blueprint,render_template,request,redirect,session
+from flask import Blueprint,render_template
 from controllers.task import add_task_function
 import sys
+from models.task import Task
 
 main = Blueprint('main',__name__)
 
 @main.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    data = Task.get_all()
+    return render_template("index.html", data=data)
 
 @main.route("/addtask", methods=["GET","POST"])
 def add_task():
     data = add_task_function()
     print(data,file=sys.stderr)
-    return render_template("addtask.html",data=data)
+    return render_template("addtask.html", data=data)
